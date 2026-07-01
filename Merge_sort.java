8e604da7b4504d1a649849c377fbf47bd29d557e
Divyanshu Singh <divyanshusingh8903@gmail.com>
2026-07-01 08:34:42 +0530

    change again

--- Merge_sort.java
+++ Merge_sort.java
@@ -1,6 +1,9 @@
 public class MergeSortTemplate {
 
-
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+        mergeSort(arr, 0, arr.length - 1);
+    }
 
     private static void mergeSort(int[] arr, int left, int right) {
         if (left >= right) return;
